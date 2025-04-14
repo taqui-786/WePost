cbab6f8e1376563cb84ac669a50307ff6b11d301
taqui-786 <mdtaqui.jhar@gmail.com>
2025-04-14 23:37:06 +0530

    Fx: some minor ui improvements

--- src/components/customComponents/ProfileFeed.tsx
+++ src/components/customComponents/ProfileFeed.tsx
@@ -6,6 +6,8 @@ import Post from "../post/Post";
 import kyInstance from "@/lib/ky";
 import InfiniteScrollingContainer from "./InfiniteScrollingContainer";
 import PostsLoadingSkeleton from "../post/PostLoadingSkeleton";
+import { Card, CardContent } from "../ui/card";
+import { User } from "lucide-react";
 interface ProfileFeedProps{
     userId:string
 }
@@ -50,9 +52,22 @@ function ProfileFeed({userId}:ProfileFeedProps) {
         <Post key={post.id} post={post} />
       ))}
       {isFetchingNextPage && <PostsLoadingSkeleton />}
-      {!hasNextPage && !posts.length && (
-        <p className="text-primary text-center">This User do not have any posts.</p>
-      )}
+      {!hasNextPage && posts.length ?  <p className="text-primary text-center">
+        Now you have reached to an end.
+      </p> : ""}
+      {!hasNextPage && !posts.length ? (
+        <Card className="w-full border-none bg-transparent shadow-none">
+          <CardContent className="flex flex-col items-center justify-center p-6 py-8 text-center">
+            <div className="bg-muted mb-3 flex h-12 w-12 items-center justify-center rounded-full">
+              <User className="text-muted-foreground h-6 w-6" />
+            </div>
+            <h4 className="mb-1 text-base font-medium">Your profile is empty</h4>
+            <p className="text-muted-foreground mb-4 text-sm">
+             This user do not have any post yet. Checkout another one!
+            </p>
+          </CardContent>
+        </Card>
+      ) : ""}
     </InfiniteScrollingContainer>
   );
 }
